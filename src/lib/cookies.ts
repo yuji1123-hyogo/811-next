ba9f9d8cdfc35136934a45c38d94d57428b4f326
LAPTOP-T546O2B1\yuji1 <yuji1123200113@gmail.com>
2025-08-11 23:14:55 +0900

    a

---
+++ src/lib/cookies.ts
@@ -0,0 +1,41 @@
+// cookie設定の型定義を作成
+// httpOnly
+// secure
+// samesite
+
+import { cookies } from "next/headers";
+
+interface CookieConfig {
+  httpOnly: boolean;
+  secure: boolean;
+  sameSite: "strict" | "lax" | "none";
+  maxAge: number;
+}
+
+// 開発環境と本番環境で設定を使い分け
+export function getCookieConfig(): CookieConfig {
+  const isProduction = process.env.Node_ENV === "production";
+
+  return {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "strict" : "lax",
+    maxAge: 365 * 24 * 60 * 60,
+  };
+}
+
+// cookieを取得
+// 解答にはセッションクッキーと書かれているが実際は永続クッキー
+export async function getCookie(): Promise<string | undefined> {
+  return (await cookies()).get("token")?.value;
+}
+
+export async function setCookie(token: string): Promise<void> {
+  "use-server";
+  const config = getCookieConfig();
+  (await cookies()).set("token", token, config);
+}
+
+export async function deleteCookie(): Promise<void> {
+  (await cookies()).delete("token");
+}
